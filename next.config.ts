cdca8e6309333f40d4be246aea587d2f8166c85d
涼·苏苏 ゙ <17089770932su@gmail.com>
2025-07-05 01:36:15 +0000

    又出现连接后端失败：API请求失败：。请检查浏览器开发者工具中的"网络"选项卡，查看请求详情，并确认ASP.NET 服务正在运行。发现proce

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:5000/api/:path*',
+        destination: 'https://api-regex.asu00131.eu.org/api/:path*',
       },
     ]
   },
